90e9b32e5d17f3f9cd501a0685f70fadf8cfbafd
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2021-11-27 12:15:09 -0800

    final commit

--- sorted.playground/Contents.swift
+++ sorted.playground/Contents.swift
@@ -11,3 +11,8 @@ func stringSimilarity(stringOne: String, stringTwo: String) -> Bool {
 func stringSimilarity2(stringOne: String, stringTwo: String) -> Bool {
     return stringOne.sorted() == stringTwo.sorted()
 }
+
+// If there is only one line of code in the function (which returns) swift knows thats the one that must return data
+func stringSimilarity3(stringOne: String, stringTwo: String) -> Bool {
+    stringOne.sorted() == stringTwo.sorted()
+}
